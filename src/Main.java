03b2d939b7beb781731ac17c324726153c6b6573
Alexandre Aragao <xandyargao19@gmail.com>
2026-03-27 20:53:33 -0300

    add readme e testes no main

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Devs;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -9,5 +11,21 @@ public class Main {
         System.out.println(c1);
         Mentoria m1 = new Mentoria("test2","descricao2", LocalDate.now());
         System.out.println(m1);
+
+        Bootcamp bootcamp1 = new Bootcamp();
+
+        bootcamp1.getConteudos().add(m1);
+        bootcamp1.getConteudos().add(c1);
+
+        Devs alexandre = new Devs();
+
+        alexandre.inscreverBootcamp(bootcamp1);
+        System.out.println("Conteúdos Inscritos Alexandre:" + alexandre.getConteudosInscritos());
+        alexandre.progredir();
+        System.out.println("Conteúdos Inscritos Alexandre:" + alexandre.getConteudosInscritos());
+        System.out.println("Conteúdos Inscritos Alexandre:" + alexandre.getConteudosConcluidos());
+        System.out.println("XP:" + alexandre.calcularTotalXp());
+
+
     }
 }
---
+++ src/README.md
@@ -0,0 +1,15 @@
+📚 Desafio DIO - Abstração em Java
+📌 Sobre
+
+Projeto em Java simulando um Bootcamp, onde desenvolvedores se inscrevem em cursos e mentorias, progridem e acumulam XP.
+
+🧠 Conceitos usados
+POO (Abstração, Herança, Encapsulamento)
+Set / LinkedHashSet
+Stream API (map, sum)
+Optional
+⚙️ Funcionalidades
+Inscrever em bootcamp
+Progredir nos conteúdos
+Separar conteúdos inscritos e concluídos
+Calcular XP total
